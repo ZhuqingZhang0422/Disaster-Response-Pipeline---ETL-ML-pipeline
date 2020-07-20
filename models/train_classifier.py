8e79b21f433029eda137629cf5463f461d567b9a
Zhuqing Zhang <68095887+ZhuqingZhang0422@users.noreply.github.com>
2020-07-19 21:59:55 -0500

    Built ML pipeline

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,96 @@
+# import libraries
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
+import re
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
+from sqlalchemy import create_engine
+
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.decomposition import TruncatedSVD
+from sklearn.ensemble import AdaBoostClassifier
+
+import pickle
 
 
 def load_data(database_filepath):
-    pass
+    #load data from 'ETL_pipeline_project'
+    engine = create_engine('sqlite:///'+database_filepath)
+    df = pd.read_sql_table(database_filepath,engine)
+    X = df['message']
+    #Remove the unrelated columns
+    Y = df.iloc[:,4:]
+    category_names = list(df[:0])[4:]
+    return X , Y , category_names
 
 
 def tokenize(text):
-    pass
+    # normalize case and remove punctuation
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+    # tokenize text
+    words = word_tokenize(text)
+    # remove stop words
+    tokens = [w for w in words if w not in stopwords.words("english")]
+    # lemmatize and stemming
+    lemmatize = [WordNetLemmatizer().lemmatize(w) for w in tokens]
+    stemmed = [PorterStemmer().stem(w) for w in lemmatize]
+    return stemmed
 
 
 def build_model():
-    pass
-
+    pipeline = Pipeline([
+    # Estimater
+    ('vect', CountVectorizer(tokenizer=tokenize)),
+    # Transformer
+    ('tfidf',TfidfTransformer()),
+    # Predictor and apply MultiOutputClassifier to assist computation
+    ('clf',MultiOutputClassifier(RandomForestClassifier()))
+])
+    # train classifier and set parameters for gridsearch
+    parameters =  {'tfidf__use_idf': (True, False), 
+              'clf__estimator__n_estimators': [50, 100], 
+              'clf__estimator__min_samples_split': [4, 8]} 
+    model = GridSearchCV(pipeline, param_grid = parameters)
+    return model
+    
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    '''
+    Evaluation report of the model, and print the F1 scores
+    Args:
+        Input: model, x_test and y_test
+        Output: evaluation report in text files 
+    '''
+    '''
+    Evaluation report of the model, and print the F1 scores
+    Args:
+        Input: model, x_test and y_test
+        Output: evaluation report in text files 
+    '''
+    y_predict = model.predict(X_test)
+    # print model prediction report
+    print(classification_report(y_predict, Y_test.values, target_names=category_names))
+    # print raw accuracy score 
+    print('F1 Score:' + np.mean(Y_test.values == y_predict))
 
 
 def save_model(model, model_filepath):
-    pass
+    # save ML model to a pickle file
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
