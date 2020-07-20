3751a6cb0d7d936451c17802b8107b200bbeaab0
Zhuqing Zhang <68095887+ZhuqingZhang0422@users.noreply.github.com>
2020-07-19 21:59:16 -0500

    Update process_data.py file for ETL pipeline

--- data/process_data.py
+++ data/process_data.py
@@ -1,27 +1,61 @@
 import sys
-
+import pandas as pd
+import sqlalchemy
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = pd.concat([messages,categories],axis = 1)
+    return df
 
 
 def clean_data(df):
-    pass
-
-
+    # create a dataframe of the 36 individual category columns
+    df_cat = df.categories.str.split(';',expand = True)
+    # select the first row of the categories dataframe
+    row = df_cat.values.tolist()[0]
+    new = []
+    
+    for char in row[0:37]:
+        new.append(char.split('-')[0])
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything 
+    # up to the second to last character of each string with slicing
+    category_colnames = new
+    
+    # rename the columns of `categories`
+    df_cat.columns = category_colnames
+    
+    # simplify contents in columns
+    for char in new:
+        df_cat[char] = df_cat[char].apply(lambda x:x.split('-')).apply(lambda y:y[1])
+    df_cat_new = df_cat
+    
+    # concatenate the original dataframe with the new `categories` dataframe
+    df.drop(['categories'], axis=1, inplace=True)
+    df = pd.concat([df, df_cat_new],axis =1 )
+    #pd.concat([df_cat['id'],df_cat.categories.str.split(';',expand = True)],axis = 1)
+    df = df.loc[:,~df.columns.duplicated()]
+    # drop duplicates
+    df.drop_duplicates(subset=None, keep='first', inplace=True)
+    return df
+    
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine('sqlite:///'+database_filename)
+    df.to_sql(database_filename, engine, index=False) 
 
 
 def main():
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
-
+      
         print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
               .format(messages_filepath, categories_filepath))
+          
         df = load_data(messages_filepath, categories_filepath)
-
+        print(df.shape)
         print('Cleaning data...')
         df = clean_data(df)
         
